287711421a3eff84b9e64bd22b8520741fe48f68
Aleksa Dimitrijevic <aleksa.dimitrijevic.business@gmail.com>
2022-02-09 00:17:51 +0100

    Updated Categories.jsx

--- webstore/src/pages/HomePage.jsx
+++ webstore/src/pages/HomePage.jsx
@@ -1,11 +1,15 @@
 import React, { Component, Fragment } from 'react';
 import FeaturedProducts from '../components/home/FeaturedProducts';
+import Categories from '../components/home/Categories';
+
 
 export class HomePage extends Component {
   render() {
     return <div>
         <Fragment>
             <FeaturedProducts/>
+            <Categories/>
+
         </Fragment>
     </div>;
   }
