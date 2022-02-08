1af996ca9bfd6c5b17602fb5fecc18f7f3df7531
Aleksa Dimitrijevic <aleksa.dimitrijevic.business@gmail.com>
2022-02-09 00:46:13 +0100

    Updated top level of the site

--- webstore/src/pages/HomePage.jsx
+++ webstore/src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import FeaturedProducts from '../components/home/FeaturedProducts';
 import Categories from '../components/home/Categories';
 import Collection from '../components/home/Collection';
 import NewArrival from '../components/home/NewArrival';
+import HomeTop from '../components/home/HomeTop';
 
 
 
@@ -10,6 +11,7 @@ export class HomePage extends Component {
   render() {
     return <div>
         <Fragment>
+            <HomeTop/>
             <FeaturedProducts/>
             <Collection/>
             <NewArrival/>
