228758fe7095d03db8ce2ba6b26d3e55eaeeac10
Aleksa Dimitrijevic <aleksa.dimitrijevic.business@gmail.com>
2022-02-09 13:20:40 +0100

    Created Contact REST API

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -3,9 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\VisitorController;
+use App\Http\Controllers\Admin\ContactController;
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
 
 Route::get('/getvisitor', [VisitorController::class, 'GetVisitorDetails']);
+Route::post('/postcontact', [ContactController::class, 'PostContactDetails']);
